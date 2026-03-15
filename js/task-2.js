6d6d2326da5cadd0a07d9c3e32264d3d69aa3e54
Natalia <164662428+Natalia-Honcharova@users.noreply.github.com>
2026-03-15 13:55:40 +0300

    hw-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,9 @@
-function getShippingMessage(country, pricede, liveryFee) {
+function getShippingMessage(country, price, deliveryFee) {
     //решение
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
+
+
 }
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
